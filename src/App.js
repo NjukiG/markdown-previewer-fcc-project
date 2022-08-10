dab469fe54e7334f5db8326e3c1920104e26fcf1
George <gnjuki19@gmail.com>
2022-08-10 18:20:42 +0300

    Build a markdown previewer for my freecodecamp certification

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { marked } from 'marked';
+
+
 
 function App() {
+  const [newText, setNewText] = useState(`
+  # H1
+  ## H2
+  [title](https://www.example.com)
+  \`code\`
+  \`\`\`
+  {
+    "firstName": "John",
+    "lastName": "Smith",
+    "age": 25
+  }
+  \`\`\`
+  1. First item
+  2. Second item
+  3. Third item
+  > blockquote
+  ![alt text](image.jpg)
+  **bold text**
+  `);
+
+  marked.setOptions({
+    breaks: true
+  })
+
+  function handleChanges(e){
+    setNewText(e.target.value)
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>MarkDown Previewer</h1>
+      <textarea rows={10} id='editor' placeholder='This is the text area' onChange={handleChanges} value={newText}></textarea>
+      <h2>The Output:</h2>
+      <div id='preview' dangerouslySetInnerHTML={{__html: marked(newText)}}></div>
+     
     </div>
   );
 }
